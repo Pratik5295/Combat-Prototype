11b0fdc8bbb8618752c5deb5c84360c4197206d2
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2018-05-26 14:27:58 -0400

    Add files via upload

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    // Use this for initialization
+
+    public int health;
+
+	void Start () {
+		
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health <= 0)
+        {
+            Death();
+        }
+       
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            //reduce health
+            Debug.Log(this.gameObject.name + " is hit by bullet.");
+            health -= 10;
+
+        }
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+}
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject Player;
+    private int playerhealth;
+    public Text playerhealthtext;
+ 
+
+	void Start () {
+
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //keep on updating the value of health
+        playerhealth = Player.GetComponent<Health>().getHealth();
+        Player_Health();
+	}
+
+
+    void Player_Health()
+    {
+        //Debug.Log(playerhealth);
+        playerhealthtext.text = "Health: " + playerhealth ;
+    }
+
+
+}
